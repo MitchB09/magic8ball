f3060a16db5bafc42ee98495e1f13f8bbd600cc4
MitchB09 <mitch.bilensky@gmail.com>
2025-10-03 21:23:41 -0300

    Allow overriding initial result

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,9 +2,21 @@ import React, { useState } from "react";
 import "./App.css";
 import getResponse from "./magic8ball";
 
+const overrideParam = 'ab'
+
 function App() {
-  const [response, setResponse] = useState<string>(getResponse());
+  const getFoutune = (): string => {
+    const params = new URLSearchParams(window.location.search);
+    if (params.has(overrideParam) && params.get(overrideParam)) {
+      return atob(params.get(overrideParam)!);
+    } else {
+      return getResponse();
+    }
+  };
+
+  const [response, setResponse] = useState<string>(getFoutune());
   const [displayed, setDisplayed] = useState<boolean>(true);
+
 
   const shakeBall = () => {
     setDisplayed(false);
@@ -12,17 +24,21 @@ function App() {
       setResponse(getResponse());
       setDisplayed(true);
     }, 1000);
-  }
+  };
 
   return (
     <div className="App">
       <div className="App-header">
-        {displayed &&
+        {displayed && (
           <>
-            <span className="fadeIn" data-testid="response">{response}</span>
-            <span onClick={shakeBall} className="askAgain">Try Again?</span>
+            <span className="fadeIn" data-testid="response">
+              {response}
+            </span>
+            <span onClick={shakeBall} className="askAgain">
+              Try Again?
+            </span>
           </>
-        }
+        )}
       </div>
     </div>
   );
